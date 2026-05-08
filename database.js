0fbec724b97f169aa94835f6ce1ce8da5ff7d831
mohammedissmaili101-gif <mohammedissmaili101@gmail.com>
2026-05-08 22:31:05 +0100

    Update database.js

--- database.js
+++ database.js
@@ -4,11 +4,17 @@ const articlesData = [
     "slug": "paris-guide.html",
     "city": "PARIS",
     "desc": "AI-curated travel intelligence and luxury patterns for 2026."
-  }
-];
+  },
   {
-    "id": Date.now(),
+    "id": 2,
     "slug": "tokyo-guide.html",
     "city": "TOKYO",
     "desc": "Neo-Zen Intelligence: Mastering the 2026 blueprint of Tokyo’s private sanctuaries and hyper-efficient luxury."
-  },
+  }
+];
+  {
+    "id": 3,
+    "slug": "london-guide.html",
+    "city": "LONDON",
+    "desc": "Sovereign Intelligence: Mastering the 2026 blueprint of London’s secret vaults and hyper-connected luxury sanctuaries."
+  }
