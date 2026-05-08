c947b9167731f4a14a692d1433c32b15f67c0411
mohammedissmaili101-gif <mohammedissmaili101@gmail.com>
2026-05-08 22:25:56 +0100

    Update database.js

--- database.js
+++ database.js
@@ -6,3 +6,9 @@ const articlesData = [
     "desc": "AI-curated travel intelligence and luxury patterns for 2026."
   }
 ];
+  {
+    "id": Date.now(),
+    "slug": "tokyo-guide.html",
+    "city": "TOKYO",
+    "desc": "Neo-Zen Intelligence: Mastering the 2026 blueprint of Tokyo’s private sanctuaries and hyper-efficient luxury."
+  },
