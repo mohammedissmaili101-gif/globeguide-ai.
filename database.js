661660f12410d08afcc466ade27919a4d5a251dd
mohammedissmaili101-gif <mohammedissmaili101@gmail.com>
2026-05-08 22:13:21 +0100

    Update and rename database.json to database.js

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
-[
+const articlesData = [
   {
     "id": 1,
     "slug": "paris-guide.html",
     "city": "PARIS",
     "desc": "AI-curated travel intelligence and luxury patterns for 2026."
   }
-]
+];
